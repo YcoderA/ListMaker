b35fa67cbb89c0e4cd15e2335ffbe34c53ca227d
Yashab Azariah <yashabazariah@gmail.com>
2022-06-22 17:04:00 -0700

    create outline for project

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
-    
+   public static void main(String[]args) {
+         run();
+   }
+    public static void run() {
+       Scanner scan = new Scanner(System.in);
+       String userInput = "n";
+       while (userInput != null && !userInput.equals("")) {
+           
+       }
+    }
 }
+
+
+
+//welcome screen
+//do you want to continue with old list or create new (2 options)
+//if want to create new create NewList
+//if want to continue with old stay on most previously used list
+//add to lists
+//take out of lists
+//should always be an option to leave list
+//make folder containing all the lists created
+//
+//
+//
